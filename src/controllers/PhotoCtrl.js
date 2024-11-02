9a328679844f21c6b0dee205649cc27ce34acd49
Eduardo Secco <dudssecco.code@gmail.com>
2024-11-02 16:17:33 -0300

    Processing Files
    Only uploads PNG and JPEG

--- src/controllers/PhotoCtrl.js
+++ src/controllers/PhotoCtrl.js
@@ -1,6 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multer'
+
+const upload = multer(multerConfig).single('file')
+
 class PhotoCtrl {
   async store(req, res){
-    res.json(req.file)
+    return upload(req, res, (err) => {
+        if(err){
+          return res.status(400).json({
+            errors: [err.code]
+          })
+        }
+        return res.json(req.file)
+    })
   }
 }
 
--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,12 +1,7 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from '../config/multer'
-
 import PhotoCtrl from '../controllers/PhotoCtrl'
 const router = new Router
 
-const upload = multer(multerConfig)
-
-router.post('/', upload.single('file'), PhotoCtrl.store)
+router.post('/', PhotoCtrl.store)
 
 export default router
