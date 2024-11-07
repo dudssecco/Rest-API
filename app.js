add03f740f558cec0148d8d0c02140994ca28c8e
Eduardo Secco <dudssecco.code@gmail.com>
2024-11-07 17:01:36 -0300

    Configure Cors Options

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ dotenv.config()
 import './src/database'
 import express from 'express'
 import { resolve } from 'path'
+import cors from 'cors'
+import helmet from 'helmet'
 
 import homeRoutes from './src/routes/homeRoutes'
 import userRoutes from './src/routes/userRoutes'
 import tokenRoutes from './src/routes/tokenRoutes'
 import alunoRoutes from './src/routes/alunoRoutes'
 import photoRoutes from './src/routes/photoRoutes'
+
+const whiteList = [
+  'https://localhost:3000'
+];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(whiteList.indexOf(origin) !== -1 || !origin){
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by cors'))
+    }
+  }
+}
 
 class App{
   constructor(){
@@ -19,6 +35,8 @@ class App{
   }
 
   middlewares(){
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({extended: true}));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')))
